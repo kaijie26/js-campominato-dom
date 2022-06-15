86f098ec195d5c293c03c0ad7556771d7791fd17
kaijie26 <chenkaijie26@gmail.com>
2022-06-15 17:47:21 +0200

    genero la griglia dopo il click

--- js/script.js
+++ js/script.js
@@ -14,6 +14,100 @@ const playBtn = document.getElementById('play-btn');
 playBtn.addEventListener( 'click', startGame);
 
 function startGame () {
+    // HTML elements
+    const myGrid = document.getElementById('grid');
+    console.log(myGrid);
+
+    // Quando l'utente inizia una nuova partita svuoto la griglia e le classi
+    myGrid.innerHTML = '';
+    myGrid.className= '';
     
 
-}
+    // Prendo il valore della difficoltà data dal utente attraverso il value
+    const userLevel = document.getElementById('difficult').value
+    console.log(userLevel);
+
+    // Definisco i vari range in base alla difficoltà
+    let rangeMaxNumber;
+    let myGridClass;
+    if(userLevel === '1') {
+        rangeMaxNumber = 100;
+        myGridClass = 'easy'
+    }else if(userLevel === '2'){
+        rangeMaxNumber = 81;
+        myGridClass = 'hard'
+    }else if(userLevel === '3'){
+        rangeMaxNumber = 49;
+        myGridClass = 'crazy'
+    }
+
+
+    // Array di bombe
+    const numBombs = 16
+    let bombs = genBombs(numBombs, 1, rangeMaxNumber);
+    console.log(bombs);
+
+    // Max numero di tentativi
+    let maxChance = rangeMaxNumber - numBombs ; 
+    console.log(maxChance);
+
+    // Array di numeri non bombe (numeri azzeccati)
+    const notBombsNum = [];
+
+    genSquare()
+
+    // Genero la griglia
+    function genSquare() {
+        // Scorro i numeri con il ciclo for in base al range che mi da l'utente
+        // <div class="square"><span>50</span></div> 
+        for(let i = 1; i <= rangeMaxNumber; i++ ) {
+            myGrid.classList.add(myGridClass);
+            // Creo i quadrati
+            const newSquare = document.createElement('div');
+            // Compilo i quadrati con i numeri in base ai range
+            newSquare.innerHTML = `<span>${i}</span>`;
+            // Aggiungo la classe al quadrato
+            newSquare.classList.add('square');
+
+            // Appendo in pagina
+            myGrid.append(newSquare);
+
+
+        }
+    
+        
+    }
+
+    
+
+
+    }
+S
+    //*********************************************************/
+    // FUNZIONI 
+    //*********************************************************************/
+
+    //--- FUNZIONE PER GENERARE BOMBE-----------------------------------------------------
+    // Genero 16 bombe del tutto casuali e li inserisco in una array ma non possono essere duplicati   
+    function genBombs(numElements, rangeMin, rangeMax) {
+
+        const arrayBombs = [];
+        while(arrayBombs.length < numElements) {
+            // Genero i numeri dal min al max in base ai range
+            const bombsElement = randomNumBomb(rangeMin, rangeMax );
+            // Inserisco nel array solo i numeri non preesenti nel array
+            if(!arrayBombs.includes(bombsElement)) {
+                arrayBombs.push(bombsElement);
+                
+                
+            }
+            
+        }
+        
+        return arrayBombs;
+    }
+
+    // Funzione che genera i numeri delle bombe randomiche
+    function randomNumBomb(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
